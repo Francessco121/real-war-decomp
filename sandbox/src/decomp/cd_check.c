eb39ddc8d6c46164eaabe251a894e3d2c0686741
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2022-09-20 20:24:25 -0400

    Decomp cd_check

---
+++ sandbox/src/decomp/cd_check.c
@@ -0,0 +1,48 @@
+#include "WINDOWS.H"
+#include "STDIO.H"
+
+char intro_cd_path[1024]; // idk what the size is
+
+long func_004d6a60(char* file_path);
+void display_message(char *format, ...);
+void game_exit();
+
+void cd_check() {
+    int retries = 0;
+    BOOL flag = FALSE;
+    FILE* file;
+
+    while (TRUE) {
+        sprintf(intro_cd_path, "%sintro.mpg", "vids\\");
+
+        if (func_004d6a60(intro_cd_path)) {
+            flag = TRUE;
+
+            cdtest:
+            sprintf(intro_cd_path, "%scdtest.txt", "vids\\");
+
+            file = fopen(intro_cd_path, "wb");
+            if (file != NULL) {
+                flag = FALSE;
+                fclose(file);
+                _unlink(intro_cd_path);
+                display_message("Please insert the Real War CD\ninto the CD rom drive and\nselect OK to continue.");
+                
+            } else {
+                if (flag) {
+                    return;
+                }
+            }
+        } else {
+            display_message("Please insert the Real War CD\ninto the CD rom drive and\nselect OK to continue.");
+
+            if (flag) {
+                goto cdtest;
+            }
+        }
+
+        if (++retries > 10) {
+            game_exit();
+        }
+    }
+}
--- sandbox/watcher/bin/watcher.dart
+++ sandbox/watcher/bin/watcher.dart
@@ -1,30 +1,52 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:path/path.dart' as p;
 import 'package:watcher/watcher.dart';
 
-const clExePath = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\VC98\\Bin\\CL.EXE';
-const clIncludePath = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\VC98\\Include';
-const dumpbinExePath = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\VC98\\Bin\\DUMPBIN.EXE';
+const vsPath = 'C:\\Program Files (x86)\\Microsoft Visual Studio';
+const clExePath = '$vsPath\\VC98\\Bin\\CL.EXE';
+const clIncludePath = '$vsPath\\VC98\\Include';
+const dumpbinExePath = '$vsPath\\VC98\\Bin\\DUMPBIN.EXE';
 final envPath = [
-  'C:\\Program Files (x86)\\Microsoft Visual Studio\\VC98\\Bin',
-  'C:\\Program Files (x86)\\Microsoft Visual Studio\\Common\\MSDev98\\Bin',
-  'C:\\Program Files (x86)\\Microsoft Visual Studio\\VC98\\Lib',
+  '$vsPath\\VC98\\Bin',
+  '$vsPath\\Common\\MSDev98\\Bin',
+  '$vsPath\\VC98\\Lib',
   Platform.environment['PATH']
 ].join(';');
 final workingDirectory = p.current;
 final srcPath = p.join(p.current, 'src');
+final objPath = p.join(p.current, 'obj');
 
-Future<void> compile(String path) async {
+String cleanPath(String path) => p.relative(path, from: workingDirectory).replaceAll('/', '\\');
+String makeObjPath(String cPath) => p.join(
+  'obj',
+  p.dirname(p.relative(cPath, from: srcPath)),
+  p.basenameWithoutExtension(cPath) + '.obj'
+);
+String makePdbPath(String cPath) => p.join(
+  'obj',
+  p.dirname(p.relative(cPath, from: srcPath)),
+  p.basenameWithoutExtension(cPath) + '.pdb'
+);
+String makeDisasmPath(String cPath) => p.join(
+  p.dirname(cPath),
+  p.basenameWithoutExtension(cPath) + '.disasm.txt'
+);
+
+Future<void> compile(String cPath) async {
   // Build command
-  path = p.relative(path, from: workingDirectory).replaceAll('/', '\\');
+  cPath = p.relative(cPath, from: workingDirectory).replaceAll('/', '\\');
 
-  final inputPath = path;
-  final objPath = p.join(p.dirname(p.dirname(path)), 'obj', p.basenameWithoutExtension(path) + '.obj');
-  final outputPath = p.join(p.dirname(path), p.basenameWithoutExtension(path) + '.disasm.txt');
+  final String objPath = makeObjPath(cPath);
+  final String pdbPath = makePdbPath(cPath);
+  final String disasmPath = makeDisasmPath(cPath);
 
-  final args = ['/nologo', '/c', '/I', clIncludePath, '/Fo$objPath', '/O1', inputPath];
+  final args = ['/nologo', '/c', '/I', clIncludePath, '/Fo$objPath', '/Fd$pdbPath', '/O2', '/Zi', cPath];
   print('[${DateTime.now()}] cl ${args.join(' ')}');
+
+  // Ensure obj directory exists
+  await Directory(p.dirname(objPath)).create();
 
   // Compile
   final result = await Process.run(clExePath, args,
@@ -32,15 +54,15 @@ Future<void> compile(String path) async {
     workingDirectory: workingDirectory,
   );
   
-  final outputFile = File(outputPath);
-  final writer = outputFile.openWrite();
+  final disasmFile = File(disasmPath);
+  final writer = disasmFile.openWrite();
   
   if (result.exitCode == -1073741515) {
     // Env is incorrect
     writer.writeln('CL.EXE returned STATUS_DLL_NOT_FOUND. Is PATH correct?');
   } else if (result.exitCode == 0) {
     // Success, disassemble the obj
-    final dumpResult = await Process.run(dumpbinExePath, ['/DISASM', '/RELOCATIONS', objPath],
+    final dumpResult = await Process.run(dumpbinExePath, ['/DISASM', '/RELOCATIONS', '/LINENUMBERS', objPath],
       environment: {'PATH': envPath},
       workingDirectory: workingDirectory,
     );
@@ -62,36 +84,72 @@ Future<void> compile(String path) async {
   await writer.close();
 }
 
-Future<bool> isOutdated(String path) async {
-  path = p.relative(path, from: workingDirectory).replaceAll('/', '\\');
+Future<bool> isOutdated(String cPath) async {
+  final String disasmPath = makeDisasmPath(cPath);
 
-  final inputPath = path;
-  final outputPath = p.join(p.dirname(path), p.basenameWithoutExtension(path) + '.disasm.txt');
+  final cFile = File(cPath);
+  final disasmFile = File(disasmPath);
 
-  final inFile = File(inputPath);
-  final outFile = File(outputPath);
-
-  if (!outFile.existsSync()) {
+  if (!disasmFile.existsSync()) {
     return true;
   }
 
-  return (await inFile.lastModified()).isAfter((await outFile.lastModified()));
+  return (await cFile.lastModified()).isAfter((await disasmFile.lastModified()));
+}
+
+Future<void> removeDisasmResult(String cPath) async {
+  final String objPath = makeObjPath(cPath);
+  final String disasmPath = makeDisasmPath(cPath);
+
+  final objFile = File(objPath);
+  final disasmFile = File(disasmPath);
+
+  if (objFile.existsSync()) {
+    print('[${DateTime.now()}] rm $objPath');
+    await objFile.delete();
+  }
+
+  if (disasmFile.existsSync()) {
+    print('[${DateTime.now()}] rm $disasmPath');
+    await disasmFile.delete();
+  }
 }
 
 Future<void> main() async {
+  // Compile outdated files
   await for (final entity in Directory(srcPath).list(recursive: true)) {
     if (entity is File && p.extension(entity.path) == '.c' && (await isOutdated(entity.path))) {
       await compile(entity.path);
     }
   }
 
-  DirectoryWatcher(srcPath).events.listen((event) {
+  // Listen for source file changes
+  final subscription = DirectoryWatcher(srcPath).events.listen((event) {
+    if (p.extension(event.path) != '.c') {
+      return;
+    }
+
+    final String cleanedPath = cleanPath(event.path);
+
     if (event.type == ChangeType.ADD || event.type == ChangeType.MODIFY) {
-      if (p.extension(event.path) == '.c') {
-        compile(event.path);
-      }
+      compile(cleanedPath);
+    } else if (event.type == ChangeType.REMOVE) {
+      removeDisasmResult(cleanedPath);
     }
   });
 
   print('Watching...');
+  
+  // Let user enter 'q' to gracefully exit (if in terminal)
+  if (stdin.hasTerminal) {
+    print('Press q to exit.');
+
+    stdin.echoMode = false;
+    stdin.lineMode =false;
+
+    await stdin.where((bytes) => utf8.decode(bytes).startsWith('q')).first;
+
+    print('Shutting down...');
+    subscription.cancel();
+  }
 }
