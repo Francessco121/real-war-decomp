8a7264f0f7486857a5bdc845253114d944a9d2cf
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2023-09-11 22:56:40 -0400

    More data.c rwmod hooks

--- rwmod/src/hooks/data_hook.c
+++ rwmod/src/hooks/data_hook.c
@@ -3,6 +3,7 @@
 #include "log.h"
 
 extern size_t _read_data_file_base(char *path, void *out);
+extern FILE *_open_data_file_base(char *path, char *mode) ;
 
 size_t read_data_file_hook(char *path, void *out) {
     int caller;
@@ -11,4 +12,13 @@ size_t read_data_file_hook(char *path, void *out) {
     log_printlnf("[%x] read_data_file(%s)", caller, path);
     
     return _read_data_file_base(path, out);
+}
+
+FILE *open_data_file_hook(char *path, char *mode)  {
+    int caller;
+    GET_CALLER_ADDRESS(caller, 12);
+    
+    log_printlnf("[%x] open_data_file(%s)", caller, path);
+    
+    return _open_data_file_base(path, mode);
 }
